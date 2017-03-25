c56fc49ab4946ea0a47a9a997014fb8da4670225
Achal Jain <achal41@gmail.com>
2017-03-25 12:45:50 +0530

    First Commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// 0: Natural, 1: Sequential, 2: Json Configured
+var tabMode = 0;
+var sampleJSON = {
+	elemSelector: String,
+	elemAttribute: Array,
+	elemchilds: Array		
+};
+
+var acs = {
+	init: function(options) {
+		console.log('Lib Init Success..');
+	},
+}
+
+//todo
+//add json
+//parse json
+//validate json
+//sample json structure
+//addelement
+//event handling
+//add/remove labels
+//properties
+
+module.exports = acs;
